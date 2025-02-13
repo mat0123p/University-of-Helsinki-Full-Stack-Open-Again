973d12872c8b169e50267f175a5f9f170297c59f
Lai Wing Hang Matthew <matthew.lai@hld.com>
2025-02-13 12:34:21 +0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const morgan = require('morgan');   
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+morgan.token('body', (req, res) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+
+let persons = 
+[
+    {
+      "id": 1,
+      "name": "Arto Hellas",
+      "number": "040-123456"
+    },
+    {
+      "id": 2,
+      "name": "Ada Lovelace",
+      "number": "39-44-5323523"
+    },
+    {
+      "id": 3,
+      "name": "Dan Abramov",
+      "number": "12-43-234345"
+    },
+    {
+      "id": 4,
+      "name": "Mary Poppendieck",
+      "number": "39-23-6423122"
+    }
+]
+
+const unknownEndpoint = (req, res) => {
+    res.status(404).send({error: 'unknown endpoint'});
+}
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons).status(200);
+});
+
+app.get('/info', (req, res) => {
+    res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`);
+}); 
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const person = persons.find(person => person.id === id);
+    if(person){
+        res.json(person);
+    }else{
+        res.status(404).end();
+    }
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
+    persons = persons.filter(person => person.id !== id);
+    res.status(204).end();
+});
+
+app.post('/api/persons', (req, res) => {
+    const personInfo = req.body;
+    if(!personInfo.name || !personInfo.number){
+        return res.status(400).json({
+            error: 'name or number is missing'
+        });
+    }
+
+    const nameExists = persons.find(person => person.name === personInfo.name);
+    if(nameExists){
+        return res.status(400).json({
+            error: 'name must be unique'
+        });
+    }
+
+    const person = {
+        id: Math.floor(Math.random() * 10000),
+        name: personInfo.name,
+        number: personInfo.number
+    };
+    persons = persons.concat(person);
+    res.json(person);
+});
+
+app.use(unknownEndpoint);
+
+PORT = 3001 || process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
